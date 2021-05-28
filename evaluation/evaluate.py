c80268733abf245a706dedcab245fc3fd2791821
Harry Mellsop <harrymellsop@gmail.com>
2021-05-29 11:54:07 +1200

    Update evaluate and inference to support commentary sampling

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -48,6 +48,37 @@ def get_prediction(game, gpt_model, stoi, itos, sample=False):
     return [itos[int(i)] for i in pred][len(game):][0]
     # TODO: MORE TO COME HERE; JUST WANT TO TEST TO SEE WHAT WE'RE GETTING OUT THE OTHER SIDE
 
+def get_commentary_prediction(game, gpt_model, stoi, itos, sample=False):
+    """
+    Extract a prediction from a given model, given a game string as a list of 
+    moves that should exist in vocab.
+    """
+
+    # if you passed in a str, then we convert it to a list
+    if isinstance(game, str):
+        game = game.rstrip().split(" ")
+    
+    # this will print out a warning if we get unk
+    x = []
+    for move in game:
+        try:
+            vocab_idx = stoi[move]
+        except KeyError:
+            print("WARNING: We've got an UNK!")
+            print("Game = ", game)
+            print("Move = ", move)
+            vocab_idx = stoi[BaseVocab().UNK]
+        x.append(vocab_idx)
+
+    # x = torch.tensor([stoi.get(c, stoi[BaseVocab().UNK]) for c in x], dtype=torch.long)[None,...].to(device)
+    x = torch.tensor(x, dtype=torch.long)[None,...].to(device)
+
+    pred = utils.sample(gpt_model, x, 100, sample=sample)[0]
+    # print(pred)
+    # print([itos[int(i)] for i in pred])
+    return [itos[int(i)] for i in pred][len(game):]
+    # TODO: MORE TO COME HERE; JUST WANT TO TEST TO SEE WHAT WE'RE GETTING OUT THE OTHER SIDE
+
 def check_end(just_moved, board, visualise):
     if visualise: vis.clear_output()
     if visualise: vis.display(board)
@@ -249,12 +280,12 @@ def analyse(results_vec, engine):
 
 def main():
     engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
-    num_trials_per_pt = 10
+    num_trials_per_pt = 100
 
     # extract parameters for pretrain chess, finetune chess, pretrain english, finetune commentary
 
     base_path = './ckpts/ckpts-through-time'
-    parameter_paths = ['pretrain_chess.pt', 'finetune_chess.pt', 'pretrain_english.pt'] # + ['finetune_commentary.pt']
+    parameter_paths = ['pretrain_chess.pt', 'finetune_chess.pt', 'pretrain_english.pt', 'finetune_commentary.pt']
 
     for path in parameter_paths:
         print(f"Analysing {path}")
