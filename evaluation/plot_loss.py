aab780b5b461540e932df7605ccfb94fd3503e52
Collin Schlager <schlagercollin@gmail.com>
2021-05-30 15:00:14 -0700

    Add argparse to plot loss function

--- evaluation/plot_loss.py
+++ evaluation/plot_loss.py
@@ -11,6 +11,7 @@ import sys
 sys.path.insert(1, '.')
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
 import torch
 import glob
 import tqdm
@@ -30,11 +31,26 @@ def get_epoch_num(ckpt_file):
     """
     return int(ckpt_file.split("epoch_")[-1].split("_iter")[0])
 
+def get_just_epoch_num(ckpt_file):
+    return int(ckpt_file.split("epoch_")[-1].split(".pt")[0])
+
 def get_epoch_and_iter(ckpt_file):
     return get_epoch_num(ckpt_file), get_iteration_num(ckpt_file)
+
+def get_sorted_checkpoints_by_epoch(ckpt_dir):
+    
+    files = glob.glob(ckpt_dir + "/epoch_[0-9].pt")
+    epochs = [get_just_epoch_num(file) for file in files]
+
+    sort_idx = np.argsort(epochs)
+    files = np.array(files)[sort_idx].tolist()
+    epochs = np.array(epochs)[sort_idx].tolist()
+
+    return epochs, files
+
     
 
-def get_sorted_checkpoints(ckpt_dir, exclude_first=True):
+def get_sorted_checkpoints(ckpt_dir):
     """
     Returns checkpoints in increasing order of iteration.
     """
@@ -47,27 +63,31 @@ def get_sorted_checkpoints(ckpt_dir, exclude_first=True):
     files = np.array(files)[np.array(sort_idx)].tolist()
     iters = np.array(iters)[np.array(sort_idx)].tolist()
 
-    if exclude_first:
-        files = files[1:]
-        iters = iters[1:]
-
     return iters, files
 
-def get_losses(ckpt_dir, exclude_first=True):
+def get_losses(ckpt_dir, exclude_zeros=True, by_epoch=False):
     """"
     Returns loss values (along with iteration number)
 
     Kwargs are those for get_sorted_checkpoints.
     """
 
-    iters, files = get_sorted_checkpoints(ckpt_dir, 
-                                          exclude_first=exclude_first)
+    if by_epoch:
+        iters, files = get_sorted_checkpoints_by_epoch(ckpt_dir)
+    else:
+        iters, files = get_sorted_checkpoints(ckpt_dir)
 
     losses = []
     for file in tqdm.tqdm(files):
 
         # map to CPU cause we don't need CUDA here
         losses.append(torch.load(file, map_location="cpu")["loss"])
+
+    losses = np.array(losses)
+    if exclude_zeros:
+        losses = losses[np.where(losses != 0)[0]]
+
+    print(losses)
 
     return iters, losses
 
@@ -77,9 +97,9 @@ def plot_losses(iters, losses):
     """
     
     global_iters = [(epoch + 1)*iteration for (epoch, iteration) in iters]
+    # global_iters = iters
 
     fig = plt.Figure()
-    print(global_iters)
     plt.plot(global_iters, losses)
     plt.xlabel("Iteration")
     plt.ylabel("Training Loss")
@@ -87,6 +107,12 @@ def plot_losses(iters, losses):
 
 
 if __name__ == "__main__":
-    ckpt_dir = "./ckpts/pretrain-english-epoch-1"
-    iters, losses = get_losses(ckpt_dir, exclude_first=True)
-    plot_losses(iters, losses)
+
+    parser = argparse.ArgumentParser(description="Plot loss from pt files.")
+    parser.add_argument('--ckpt_dir', type=str, help='pt directory')
+    args = parser.parse_args()
+
+    iters, losses = get_losses(args.ckpt_dir, 
+                               exclude_zeros=True,
+                               by_epoch=False)
+    plot_losses(iters, losses, by_epoch=False)
